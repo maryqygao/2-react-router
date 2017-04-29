a8e34974240c4e238b05d8d25a2a0b44f3f6f9cb
Mary Gao <maryqygao@gmail.com>
2017-04-28 19:52:28 -0700

    Delete placeholder code for testing css modules and globals

--- src/js/client.js
+++ src/js/client.js
@@ -3,18 +3,5 @@ import ReactDOM from 'react-dom';
 
 import Layout from './pages/Layout';
 
-import styles from '../css/modules.mscss';
-
-let element = `
-  <div class="${styles.element}">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur laudantium recusandae itaque libero velit minus ex reiciendis veniam. Eligendi modi sint delectus beatae nemo provident ratione maiores, voluptatibus a tempore!</p>
-  </div>
-  <div class='bg-inverse'>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur laudantium recusandae itaque libero velit minus ex reiciendis veniam. Eligendi modi sint delectus beatae nemo provident ratione maiores, voluptatibus a tempore!</p>
-  </div>
-`;
-
-document.write(element);
-
 const app = document.getElementById('app');
 ReactDOM.render(<Layout />, app);
